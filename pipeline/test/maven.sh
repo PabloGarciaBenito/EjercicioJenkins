a0933c99c9a08a434cf449e289965577dba692b0
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-13 09:38:12 +0100

    agregados scripts para construir el jar y hacer los tests

---
+++ pipeline/test/maven.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "***************************"
+echo "** Testing the code ***********"
+echo "***************************"
+
+WORKSPACE=/home/jenkins/jenkins/jenkins_home/workspace/pipeline-docker-maven/
+docker run --rm -v $WORKSPACE/java-app:/app -v /var/jenkins_home/.m2:/root/.m2 -w /app maven:3-alpine "$@"
