9cd8ba34958c8643831d6b99bc9101d17e1576a4
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-13 09:49:33 +0100

    Scripts arreglados

--- pipeline/test/maven.sh
+++ pipeline/test/maven.sh
@@ -4,5 +4,5 @@ echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins/jenkins_home/workspace/pipeline-docker-maven/
+WORKSPACE=/home/jenkins/jenkins/jenkins_home/workspace/Ejercicio/
 docker run --rm -v $WORKSPACE/java-app:/app -v /var/jenkins_home/.m2:/root/.m2 -w /app maven:3-alpine "$@"
